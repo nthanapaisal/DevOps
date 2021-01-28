78aa9ab8d2991ff5ff7094b2af4f3556b4df5454
kvk745 <kvk745@my.utsa.edu>
2021-01-28 14:02:15 -0600

    added node starter project

---
+++ README.md
@@ -0,0 +1,16 @@
+#### Configuration
+<p>
+server.js expects a .env that contains:
+<br />
+LISTEN_PORT={desired port number}
+</p>
+
+#### Building
+<p>
+npm i
+</p>
+
+#### Running
+<p>
+npm run dev
+</p>
---
+++ gateways/propertydb.js
@@ -0,0 +1,8 @@
+const utilities = require("../misc/utilities");
+const logger = utilities.getLogger();
+
+module.exports = {
+	fetchProperties: () => {
+		return "here is a bunch of properties for you";
+	}
+};
