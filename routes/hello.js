7df9dea25206f2611e89ddbb22cd2f64ad6eaf1e
nthanapaisal <nikkithanapaisal@gmail.com>
2021-02-05 14:02:19 -0600

    updated /hellp endpoint

--- routes/hello.js
+++ routes/hello.js
@@ -16,7 +16,7 @@ router.get('/',
   async function(request, response) {
         logger.info("success");
 
-        utilities.sendResponse(response, 200, "return hello response here");
+        utilities.sendResponse(response, 200, "Hello yourself");
     }
 );
 
