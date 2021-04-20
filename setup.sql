4590659051a0bde9e8cede2a9a7ab7610c1c788c
kvk745 <kvk745@my.utsa.edu>
2021-04-19 23:59:57 -0500

    finished docker-compose up deployment

--- setup.sql
+++ setup.sql
@@ -5,8 +5,8 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE IF NOT EXISTS `cs4783_A4` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `cs4783_A4`;
+CREATE DATABASE IF NOT EXISTS `cs4783_iaf873` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cs4783_iaf873`;
 
 CREATE TABLE IF NOT EXISTS `property` (
   `property_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS `property` (
   `zip` char(10) NOT NULL,
   `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`property_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*make sure your API connect db not as root */
-GRANT SELECT,UPDATE,DELETE,INSERT on cs4783_fru574.* to `fru574`@`%` identified by 'secretPassword';
+GRANT SELECT,UPDATE,DELETE,INSERT on cs4783_iaf873.* to `fru574`@`%` identified by 'secretPassword';
